55530e23ded7cd6b7caf67881d1d156e8f273834
georgee <jurajbosy1@gmail.com>
2023-04-30 12:03:20 +0200

    feat: setup main branch

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+	<div class="container d-flex flex-column justify-content-center">
+		<h1 class="text-center">Cinematic <span>Score</span></h1>
+		<div class="description-container mt-5">
+			<ul>
+				<li>
+					Introducing our upcoming cinematic score website where movie
+					enthusiasts can rate and review their favorite films with ease.
+				</li>
+				<li>
+					Stay tuned for the official launch of our user-friendly platform
+					designed to enhance your movie-watching experience.
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
